1981bf696768ea0b3805a84cd9c73c9a5eeefa86
차준호 <auio127@gmail.com>
2023-02-17 02:12:29 +0900

    commit
    Before start DB

--- src/server.js
+++ src/server.js
@@ -9,15 +9,15 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views"); //기본값이 ./views이기때문에 현재 작업 디렉토리에 /src/views를 add
 app.use(logger);
+app.use(express.urlencoded({ extended: true })); //express App이 form의 value를 이해할 수 있게 한다.
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 const handleListening = () =>
- console.log(`✔ Server listening on port http://localhost:${PORT} 🎃`);
+  console.log(`✔ Server listening on port http://localhost:${PORT} 🎃`);
 
 app.listen(PORT, handleListening);
